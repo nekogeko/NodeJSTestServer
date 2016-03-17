d7c6d63aa338f54cf5ae6c8f07abcb03f1c00efb
EC2 Default User <ec2-user@ip-172-31-12-143.ec2.internal>
2016-03-17 19:03:08 +0000

    Replaced sys by console

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-var sys = require("sys"),
+var console = require("console"),
 my_http = require("http");
 my_http.createServer(function(request,response){
-    sys.puts("I got kicked");
+    console.log("I got kicked");
     response.writeHeader(200, {"Content-Type": "text/plain"});
     response.write("Hello World");
     response.end();
 }).listen(8080);
-sys.puts("Server Running on 8080"); 
+console.log("Server Running on 8080"); 
