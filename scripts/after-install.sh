dc5dcbf725f0f2f66ff48c7a5659a9e3e3d92de6
EC2 Default User <ec2-user@ip-172-31-12-143.ec2.internal>
2016-03-17 18:07:09 +0000

    Fixing issue with after-install deployment script

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-NODE="/home/ec2-user/node-v5.8.0-linux-x64/bin"
+NODE="/home/ec2-user/node-v5.9.0-linux-x64/bin"
 echo "Setting node folder to $NODE"
 $NODE/node --version
 echo "Installing typings and webpack" >> after-install.txt
