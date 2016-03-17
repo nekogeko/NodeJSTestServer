882b488ae8de451b145b44c9190eaaf4a04cfa53
EC2 Default User <ec2-user@ip-172-31-12-143.ec2.internal>
2016-03-17 18:14:08 +0000

    123

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -2,8 +2,8 @@
 NODE="/home/ec2-user/node-v5.9.0-linux-x64/bin"
 echo "Setting node folder to $NODE"
 $NODE/node --version
-echo "Installing typings and webpack" >> after-install.txt
+echo "Installing typings and webpack"
 $NODE/npm install webpack typings webpack-dev-server -g
-echo "Installing dependencies" >> after-install.txt
+echo "Installing dependencies"
 $NODE/npm install
-echo "After installation completed." >> after-install.txt
+echo "After installation completed."
