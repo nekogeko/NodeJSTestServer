0fdb8b3deea71a42c66d89052b15a0e703fd1c9f
Nicolas RICHARD <nicholas.richard@gmail.com>
2016-03-21 19:16:02 +0000

    Updated verification script

--- scripts/validate-service.sh
+++ scripts/validate-service.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-RESULT="$(curl localhost:8080)"
-echo "Deployment result: $RESULT"
+result=$(curl -s -I http:// localhost:8080)
+if [[ "$result" == *"200 OK"* ]]; then
+  echo "Deployment is a success"
+  exit 0
+else
+  echo "Deployment failed"
+  exit 1
+fi
