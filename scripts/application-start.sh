f941192a629131eb81f57a56c8469fa6e0c1113f
Nicolas RICHARD <nicholas.richard@gmail.com>
2016-03-21 16:42:44 +0000

    Testing

--- scripts/application-start.sh
+++ scripts/application-start.sh
@@ -2,4 +2,5 @@
 FOLDER=/tmp/Angular2CodeDeploy
 cd $FOLDER
 echo "Running application"
+NODE="/home/ec2-user/node-v5.9.0-linux-x64/bin"
 $NODE/forever start -al ./forever.log -ao out.log -ae err.log ./server.js
--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -3,7 +3,11 @@ export FOLDER=/tmp/Angular2CodeDeploy
 echo "Creating application folder $FOLDER"
 mkdir -p $FOLDER
 cd $FOLDER
-export NODE="/home/ec2-user/node-v5.9.0-linux-x64/bin"
+echo "Installing NodeJS"
+cd ~
+rm -Rf ~/node-v5.9.0-linux-x64
+tar -xzf ~/node-v5.9.0-linux-x64.tar.gz
+export NODE=/home/ec2-user/node-v5.9.0-linux-x64/bin
 echo "Setting node folder to $NODE"
 export PATH=$PATH:$NODE
 $NODE/node --version
@@ -12,6 +16,6 @@ $NODE/npm install forever -g
 echo "Installing typings and webpack"
 $NODE/npm install webpack typings webpack-dev-server -g
 echo "Killing existing Node process"
-forever stopall
+$NODE/forever stopall
 echo "After installation completed."
 
