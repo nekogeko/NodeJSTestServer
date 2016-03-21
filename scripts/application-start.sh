a1137ed211592b9cbe98e6d4ff23785c909f195b
EC2 Default User <ec2-user@ip-172-31-59-61.ec2.internal>
2016-03-21 14:37:20 +0000

    Making application run script not tied to console

--- scripts/application-start.sh
+++ scripts/application-start.sh
@@ -3,4 +3,4 @@ export FOLDER=/tmp/Angular2CodeDeploy
 NODE="/home/ec2-user/node-v5.9.0-linux-x64/bin"
 cd $FOLDER
 echo "Running application"
-$NODE/node server.js &
+sudo nohup $NODE/node server.js &
