21a219d032a34418893aa5c9094b520afd5e17bc
nirichar <nirichar@teksystems.com>
2016-03-24 11:08:50 -0400

    Switching output to HTML

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ var console = require("console"),
 my_http = require("http");
 my_http.createServer(function(request,response){
     console.log("I got kicked");
-    response.writeHeader(200, {"Content-Type": "text/plain"});
-    response.write("Hello World - Version 2.1.1 <<<<<<<<<<WEEEEEEEEEE>>>>>>>> with CodePipeline !");
+    response.writeHeader(200, {"Content-Type": "text/html"});
+    response.write('<html><head><body><h1>Hello World - Version 2.1.1</h1>' +
+        '<p/>' +
+        'Here is a sabre for you:<br/>' +
+        '<img src="http://i.imgur.com/HfkfEZc.jpg?1" width="300px">' +
+        '</body>');
     response.end();
 }).listen(8080);
 console.log("Server Running on 8080"); 
